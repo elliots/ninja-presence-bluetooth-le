2c2d897f4751e035eb8307a1fd605bc9db755765
Elliot Shepherd <elliot@jarofworms.com>
2013-05-30 23:16:11 +1000

    Initial version

---
+++ index.js
@@ -0,0 +1,37 @@
+var Presence = require('ninja-presence-base');
+
+
+module.exports = Presence;
+
+Presence.prototype.G = 'bluetoothle';
+Presence.prototype.scan = function() {
+  var self = this;
+
+  var noble = require('noble');
+
+  noble.on('stateChange', function(state) {
+    self._app.log.debug('State change', state);
+    if (state === 'poweredOn') {
+
+      noble.startScanning();
+    } else {
+      noble.stopScanning();
+    }
+  });
+
+  noble.on('scanStart', function() {
+    self._app.log.info('Starting scan');
+  });
+
+  noble.on('scanStop', function() {
+    self._app.log.info('Stopping scan');
+  });
+  noble.on('discover', function(peripheral) {
+      self.see({
+            name: peripheral.advertisement.localName,
+            id: peripheral.uuid,
+            distance: Math.abs(peripheral.rssi)
+      });
+  });
+
+};
---
+++ test.js
@@ -0,0 +1,26 @@
+var opts = {};
+
+var d = new (require('index'))(opts, {
+    on : function(x,cb){
+        setTimeout(cb, 100);
+    },
+    log: {
+        debug: console.log,
+        info: console.log,
+        warn: console.log,
+        error: console.log
+    }
+});
+
+d.emit = function(channel, value) {
+    console.log('Driver.emit', channel, value);
+    if (channel == 'register') {
+        value.emit = function(channel, value) {
+            console.log('Device.emit', channel, value);
+        };
+    }
+};
+
+d.save = function() {
+    console.log('Saved opts', opts);
+};
